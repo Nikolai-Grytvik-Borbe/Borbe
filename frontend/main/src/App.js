e52c7ef19d3f22a05896a5ec84d6f28ca58cebab
Nikolai <bus.nikolai@gmail.com>
2022-07-03 23:20:52 +0200

    Practice with website design

---
+++ frontend/main/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Borbe.no</h1>
+      </header>
+      <main>
+        <h3 className="App-text">A portfolio/blog project by Fredrik and Nikolai Borbe</h3>
+      </main>
+      <footer className="footer">
+        <small>© 2022 Nikolai Borbe, Fredrik Borbe</small>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
