997729a73e3a9649f660cbcb81cd2d072d7e0232
Nikolai <bus.nikolai@gmail.com>
2022-07-03 14:39:35 +0200

    Practice: Creating a list using React

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 function homepage() {
     ReactDOM.render(<h1>Hello There</h1>, document.getElementById("title"))
     ReactDOM.render(<p>A website is appearing</p>, document.getElementById("subtext"))
+    ReactDOM.render([<li>Banana</li>, <li>Apple</li>, <li>Oranges</li>, <li>Kiwis</li>], document.getElementById("list"))
 }
 
 homepage()
