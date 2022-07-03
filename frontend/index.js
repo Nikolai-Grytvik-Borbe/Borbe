4d499bdcb45fb5e0954d8910ad71db1c5df56685
Nikolai <bus.nikolai@gmail.com>
2022-07-03 20:32:26 +0200

    TODO liste for nettsiden

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,18 @@
-function homepage() {
-    ReactDOM.render(<h1>Hello There</h1>, document.getElementById("title"))
-    ReactDOM.render(<p>A website is appearing</p>, document.getElementById("subtext"))
-    ReactDOM.render([<li>Banana</li>, <li>Apple</li>, <li>Oranges</li>, <li>Kiwis</li>], document.getElementById("list"))
-}
+const app = (
+    <div>
+        <a href="https://www.youtube.com/watch?v=OAUtiqYDhEc">
+            <img className="photo" src="media/logo.png" alt="LOGO"></img>
+        </a>
+        <center><h1 className="title">TODO</h1></center>
+        <hr />
+        <h2>Borbe Nettside</h2>
+        <h3>Har ingen liste, så her en noen funfacts om norge:</h3>
+        <ul className="list">
+            <li>Norge har den lengste tunellen</li>
+            <li>Norge introduserte lakse sushi til Japan</li>
+            <li>Den øverste oberst for den norske kongegarden er en skotsk pingvin</li>
+        </ul>
+    </div>
+)
 
-homepage()
+ReactDOM.render(app, document.getElementById("root"))
