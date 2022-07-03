ebde4a00a9e89fe6711421d1b79f877a687963f0
Fredrik Borbe <fredrik.borbe@gmail.com>
2022-07-03 14:26:03 +0200

    fiksa secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
+path_to_key = "../.env/secret.txt"
+with open(path_to_key) as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
