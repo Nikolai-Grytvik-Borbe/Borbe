75a54be8ccca704d5064309defaca2cf8609b3a6
Nikolai <bus.nikolai@gmail.com>
2022-07-04 14:15:44 +0200

    v.0.0.1 Layout Homepage design

--- frontend/main/src/App.js
+++ frontend/main/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Borbe.no</h1>
+//       </header>
+//       <main>
+//         <h3 className="App-text">A portfolio/blog project by Fredrik and Nikolai Borbe</h3>
+//       </main>
+//       <footer className="footer">
+//         <small>© 2022 Fredrik Borbe, Nikolai Borbe, </small>
+//       </footer>
+//     </div>
+//   );
+// }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Borbe.no</h1>
-      </header>
-      <main>
-        <h3 className="App-text">A portfolio/blog project by Fredrik and Nikolai Borbe</h3>
-      </main>
-      <footer className="footer">
-        <small>© 2022 Fredrik Borbe, Nikolai Borbe, </small>
-      </footer>
-    </div>
-  );
+  return(
+    <>
+      <div class="split left">
+        <div class="centered">
+          <h2>Nikolai</h2>
+          <p>Some text.</p>
+        </div>
+      </div>
+
+      <div class="split right">
+        <div class="centered">
+          <h2>Fredrik</h2>
+          <p>Some text here too.</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
